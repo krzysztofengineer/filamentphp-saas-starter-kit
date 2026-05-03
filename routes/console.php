74c0f22401847942748d06a8a08b24413aa7097c
Krzysztof <krzysztof@engineer.com>
2026-05-03 17:29:07 +0200

    Update scheduled user pruning command to use PruneDeletedUsers class for improved clarity and maintainability

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Console\Commands\PruneDeletedUsers;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -8,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('users:prune')->daily();
+Schedule::command(PruneDeletedUsers::class)->daily();
